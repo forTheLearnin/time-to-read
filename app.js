d370076745f1443a6d27468d2e10307a2e862e33
Blake Guilloud <fbguillo@gmail.com>
2017-01-30 21:32:19 -0500

    Initial commit

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
-const wordArray = document.getElementById('content');
-
 const form = document.getElementById('form');
+
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  const newWPM = parseInt(event.target[0].value);
-
-  setState(newWPM);
+  setState(fetchCurrentWPM());
 });
 
+function fetchCurrentWPM(args) {
+  const input = document.getElementById('input');
+
+  const newWPM = parseInt(input.value || args);
+
+  return newWPM;
+}
 
 function readTime(args) {
-  return Math.round(getMean());
+  return Math.round(getMean(args));
+}
 
-  function getMean() {
-    return wordArray.innerHTML.length / args;
-  }
+function getMean(args) {
+  const wordArray = document.getElementById('content');
+
+  return wordArray.value.split(' ').length / args;
 }
 
 function setState(args) {
   document.getElementById('readTime').innerHTML = readTime(args || 230);
+  document.getElementById('currentWPM').innerHTML = fetchCurrentWPM(230);
 }
 
 setState();
